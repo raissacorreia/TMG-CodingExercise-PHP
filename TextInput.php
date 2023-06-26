05fe5e49483806fb891135a1f28713deded61bd6
Raissa Correia <raissa.correia@pyyne.com>
2023-06-26 16:39:19 -0300

    starting docblock comments

--- TextInput.php
+++ TextInput.php
@@ -2,18 +2,24 @@
 require_once 'Input.php';
 class TextInput extends Input
 {
-    /* Implement the validation logic for the TextInput
-    * You can customize the validation rules according to your requirements
-    */
+    /**
+     * Validates the input value.
+     *
+     * This method ensure this textfield is not empty.
+     *
+     * @return bool Returns true if the input value is not empty, false otherwise.
+     */
     public function validate()
     {
         return !empty($this->_initVal);
     }
 
-    /* Implement the rendering of any additional settings for the TextInput
-     * This method is intended to be used by subclasses for customization
-     * You can add additional HTML markup or settings specific to the TextInput
-     * Example: Render any additional attributes or settings for the TextInput
+    /**
+     * Renders any additional settings for the TextInput.
+     *
+     * This method is intended to be used by subclasses for customization.
+     * Subclasses can override this method to add additional HTML markup or settings specific to the TextInput.
+     * For example, they can render any additional attributes or settings for the TextInput.
      */
     protected function _renderSetting()
     {
@@ -22,6 +28,15 @@ class TextInput extends Input
             '" class="custom-class">';
     }
 
+    /**
+     * Constructor for the Input class.
+     *
+     * Initializes a new Input object with the given name, label, and initial value.
+     *
+     * @param string $name The name of the input.
+     * @param string $label The label of the input.
+     * @param string $value The initial value of the input (optional, defaults to an empty string).
+     */
     public function __construct($name, $label, $value = '')
     {
         $this->_name = $name;
@@ -29,6 +44,11 @@ class TextInput extends Input
         $this->_initVal = $value;
     }
 
+    /**
+     * Renders the HTML and CSS for the TextInput element.
+     *
+     * @return void
+     */
     public function render()
     {
         echo '
